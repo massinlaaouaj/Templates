a5a57fc44f39d37ac4854d3b3d40aafe5a7d6f33
massinlaaouaj <massin.laaouaj@gmail.com>
2020-03-25 13:27:26 +0100

    ----

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Templates
 Templates for daily use, of different programming languages, in order to simplify the work.
+
+
+## List:
+<hr>
+
+### `Linux and Windows`:
+- [Simple index.html to start](html/index.html)
+
+
+### `Windows`:
+- [Simple index.html to start](html/index.html)
+
+
+### `🐧Linux`:
+
+- [Block or unblock ip address](bash/block_unblock_ip.sh)
+- [Show the largest 10 currently open files, the size of those files in Megabytes, and the name of the process holding the file open](commands/Linux/10_largest_files.sh)
+- [Install Docker and Docker-compose](docker/install_docker.sh)
+- [Purge Docker](docker/purge_docker.sh)
+- [Configure git global](git/configure_git.sh)
+- [Simple index.html to start](html/index.html)
---
+++ bash/block_unblock_ip.sh
@@ -0,0 +1,57 @@
+#If you wish to block an ip address for whatever reason:
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please use sudo, or login as root"
+  exit
+fi
+
+RED='\033[0;41;30m'
+MESSAGE='\e[43mYellow'
+ACTION='\e[1;32m'
+STD='\033[0;0;39m'
+
+MAX=5
+I=1
+
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~"	
+	echo "         MENU:       "
+	echo "~~~~~~~~~~~~~~~~~~~~~"
+	echo "0. Block IP address"
+	echo "1. Unblock IP address"
+	echo "2. List iptables"
+	echo "q. Exit"
+}
+
+while [ $I -le $MAX ]
+	do
+		show_menus
+		read -p "Enter choise> " OPTION
+
+		case "$OPTION" in
+
+			"0") 	echo "\n Write the IP you want to block:"
+				read -p "> " IPADDRBLOCK
+				$( iptables -A INPUT -s $IPADDRBLOCK -j DROP )
+				echo "${MESSAGE}The IP $IPADDRBLOCK is blocked${STD}" && sleep 1
+				echo ""
+			;;
+			"1")	echo "\nWrite the IP you want to unblock:"
+				read -p "> " IPADDRUNBLOCK
+				$( iptables -D INPUT -s $IPADDRUNBLOCK -j DROP )
+				echo "${MESSAGE}The IP $IPADDRBLOCK is unblocked${STD}" && sleep 1
+				echo ""
+			;;
+			"2")	echo "\nList:"
+				echo "$( iptables -L )"
+				echo "\n${ACTION}[Enter to continous]${STD}"
+				read ENTER
+			;;
+			"q")	exit
+			;;
+			*)	echo -e "${RED}Error...${STD}" && sleep 1
+				I=$((I+1))
+			;;
+		esac
+	done
